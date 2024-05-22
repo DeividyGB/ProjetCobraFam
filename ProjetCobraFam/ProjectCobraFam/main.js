b915ca48e43ad3e04a55557044b7ab1f900f341c
DeividyGB <134128576+DeividyGB@users.noreply.github.com>
2024-05-22 20:05:25 -0300

    Add files via upload

---
+++ ProjetCobraFam/ProjectCobraFam/main.js
@@ -0,0 +1,24 @@
+function toggleSidebar() {
+    var sidebar = document.getElementById("sidebar");
+    var overlay = document.getElementById("overlay");
+    
+    sidebar.classList.toggle("open");
+    overlay.classList.toggle("show");
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    var menuButton = document.querySelector(".menu-button");
+    var closeButton = document.querySelector(".close-button");
+    var overlay = document.getElementById("overlay");
+    
+    menuButton.addEventListener("click", toggleSidebar);
+    closeButton.addEventListener("click", toggleSidebar);
+    overlay.addEventListener("click", toggleSidebar);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    let div = document.createElement("div");
+    div.style = "Bababoe";
+    
+})
+
